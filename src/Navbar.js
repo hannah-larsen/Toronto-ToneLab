78f4c11175fcc8996395bb12d4c4d414396d5da8
Hannah Larsen <18hkl@queensu.ca>
2025-01-03 22:05:52 -0500

    app refresh with new theme, removed old content

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './globals.css';
+import React from "react";
+import "./globals.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        {/*<button className="mute-button">Mute</button>*/}
-        <h1 className="navbar-title">SoundBytes </h1>
+        <h1 className="navbar-title">Toronto ToneLab</h1>
       </div>
     </nav>
   );
