cf5c1437aa0562cbd1f058988434332bce9ebabd
Hannah Larsen <18hkl@queensu.ca>
2025-01-04 00:05:55 -0500

    updated UI, added placeholder audio

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./globals.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-text">Hannah Larsen @ 2025</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
