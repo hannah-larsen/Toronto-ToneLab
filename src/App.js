5085825066d9e1092b59bd490cd28ed5deaa211a
Hannah Larsen <18hkl@queensu.ca>
2025-03-02 15:52:38 -0500

    added line 1/line 2 audios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./Footer";
 import "./App.css";
 import TorontoLineArt from "./assets/toronto-line-art.png";
 import TTCAmbience from "./sounds/ttc-ambience-loop.mp3";
+import Line1 from "./sounds/Line1.mp3";
+import Line2 from "./sounds/Line2.mp3";
 
 function App() {
   return (
@@ -31,6 +33,46 @@ function App() {
               <source
                 className="Audio-Player"
                 src={TTCAmbience}
+                type="audio/mpeg"
+              ></source>
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        </div>
+
+        <div className="Audio-Container">
+          <div>
+            <p className="Audio-Title">Line 1</p>
+          </div>
+          <div class="audio-player">
+            <audio
+              controls
+              controlsList="nodownload noplaybackrate"
+              loop="true"
+            >
+              <source
+                className="Audio-Player"
+                src={Line1}
+                type="audio/mpeg"
+              ></source>
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        </div>
+
+        <div className="Audio-Container">
+          <div>
+            <p className="Audio-Title">Line 2</p>
+          </div>
+          <div class="audio-player">
+            <audio
+              controls
+              controlsList="nodownload noplaybackrate"
+              loop="true"
+            >
+              <source
+                className="Audio-Player"
+                src={Line2}
                 type="audio/mpeg"
               ></source>
               Your browser does not support the audio element.
